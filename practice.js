0609a2770b041cced35e3ce472c48fef926028c7
Adrienne Wootton <adrienne@Owners-MacBook-Pro.local>
2019-11-03 23:53:28 -0700

    javascript 2.1

--- practice.js
+++ practice.js
@@ -13,8 +13,12 @@
   Give it a key of name with the value being your name, and another key of age with the value being your age.
   Then alert your name using dot notation.
 */
+var me = {
+  name: "Adrienne Wootton",
+  age: 35
+}
 
-//Code here
+// alert(me.name)
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +27,30 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "NF",
+  food: "Pasta",
+  person: "Jesse",
+  book: "Four Agreements",
+  movie: "Dumb & Dumber",
+  holiday: "Birthday"
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
-
-//Code here
+favoriteThings.car = "4Runner";
+favoriteThings.brand = "Toyota";
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
+
 
 ////////// PROBLEM 3 //////////
 
@@ -48,14 +61,17 @@
   Set the value of that key to 'chapstick'.
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
-
-//Code here
+ var backPack = {}
+   item = 'firstPocket'
+backPack.firstPocket = 'chapstick'
+backPack.color = 'black'
+ 
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +79,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack)
 
 ////////// PROBLEM 4 //////////
 
@@ -84,7 +100,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
+user2.name = 'Bryan G. Smith';
+user2.email = 'bryan.smith@devmounta.in';
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
 ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
@@ -127,3 +144,4 @@ var user2 = {
 */
 
 //Code Here
+ 
